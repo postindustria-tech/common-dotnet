896a3500a61ea8969be3668ed6386595fe35a03d
ben51degrees <ben@51degrees.com>
2024-05-28 11:59:26 +0100

    TEST: Renamed tests project to match convension.

--- FiftyOne.Common.Test/LimitedHttpClientTests.cs
+++ FiftyOne.Common.Test/LimitedHttpClientTests.cs
@@ -7,7 +7,7 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace FiftyOne.Common.Test
+namespace FiftyOne.Common.Tests
 {
     [TestClass]
     public class LimitedHttpClientTests
